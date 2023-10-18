21159bec1c33f9ce5d72e441497aafc3f799a56c
Simon <Simon.kaufmann1997@gmail.com>
2023-10-18 23:10:35 +0200

    File Anpassung

--- Install-IPPrinter.ps1
+++ Install-IPPrinter.ps1
@@ -16,7 +16,7 @@
 
  
 .EXAMPLE
-   Install-IPPrinter.ps1 -PortIPAddress "192.168.1.10" -PrinterName "Drucker 1. OG" -DriverName "HP Universal Printing PCL 6" -DriverInfFileName "hpcu255u.inf"
+   Install-IPPrinter.ps1 -PortIPAddress "192.168.1.10" -PrinterName "Drucker 1. OG" -DriverName "HP Universal Printing PCL 6" -DriverInfFileName "hpcu270u.inf"
  
 .PARAMETER PortIPAddress
    Die IP-Adresse des Druckers.
